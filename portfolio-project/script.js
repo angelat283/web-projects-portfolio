b4be54d272855f52258f9240096f2c62f2f2953b
zainab angel suleiman <angelatane123@gmail.com>
2025-12-20 11:38:53 -0500

    Update script.js

--- portfolio-project/script.js
+++ portfolio-project/script.js
@@ -1,4 +1,11 @@
 document.getElementById("btn").addEventListener("click", function () {
   const message = document.getElementById("message");
-  message.classList.toggle("hidden");
+
+  if (message.classList.contains("hidden")) {
+    message.classList.remove("hidden");
+    this.textContent = "Hide Project Details";
+  } else {
+    message.classList.add("hidden");
+    this.textContent = "View Project Details";
+  }
 });
